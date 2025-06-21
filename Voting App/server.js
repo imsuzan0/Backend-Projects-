17ffdbd79615a4393612b8bd275b722a2ef92de2
Sujan Timalsina <sujantimalsina333@gmail.com>
2025-06-21 14:07:05 +0545

    feat: add user and candidate routes to server.js

--- Voting App/server.js
+++ Voting App/server.js
@@ -1,12 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
-import { connectDB } from "./db/db";
+import { connectDB } from "./db/db.js";
+import userRouter from "./routes/user.route.js";
+import candidateRouter from "./routes/candidate.route.js";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json({ limit: "10mb", extended: true }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use("/api/user",userRouter)
+app.use("/api/candidate",candidateRouter)
 
 const PORT = process.env.PORT || 3000;
 
